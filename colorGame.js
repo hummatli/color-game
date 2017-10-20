abf4ccae3108eb4e49d04c855f7b21e8e353b4f9
Sattar Hummatli <settarxan@gmail.com>
2017-10-20 11:14:45 +0400

    Added functionality random color pick

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,9 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6)
 
 var squares = document.querySelectorAll(".square")
 var colorDisplay = document.getElementById("colorDisplay")
 var messageDisplay = document.getElementById("message")
+var h1 = document.querySelector("h1")
 var pickedColor = pickColor()
 
 
@@ -38,6 +32,7 @@ for(var i =0; i < squares.length; i++) {
 
 function changeColors(color) {
 
+	h1.style.backgroundColor = color
 	for(var i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = color
 	}
@@ -48,3 +43,31 @@ function pickColor() {
 	var random = Math.floor(Math.random() * colors.length)
 	return colors[random]
 }
+
+function generateRandomColors(num) {
+	//make an array
+	var arr = []
+
+	//add num random colors to array
+	for(var i = 0; i < num; i++) {
+		arr.push(randomColor())
+	}
+	
+	//return array
+	return arr
+}
+
+function randomColor() {
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256)
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256)
+	//pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256)
+
+	return "rgb(" + r + ", " + g + ", " + b + ")"
+}
+
+
+
+
